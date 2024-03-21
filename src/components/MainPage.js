a9c7da22e6c57ce224b8f9ecb3bbba78dae8cded
AnamulAmin <161101326+AnamulAmin@users.noreply.github.com>
2024-03-21 12:13:59 -0700

    Add files via upload
    added mainPage component

--- src/components/MainPage.js
+++ src/components/MainPage.js
@@ -1,18 +1,20 @@
 "use client";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import VideoArea from "./VideoArea";
 import ChatBox from "./ChatBox";
 import ParticipantFormModal from "./ParticipantFormModal";
 
 function MainPage(props) {
   const [user, setUser] = useState(null);
+  const [player, setPlayer] = useState(null);
+
   return (
     <main className="grid grid-cols-1 sm:grid-cols-3 gap-6">
       <div className="col-span-2">
-        <VideoArea />
+        <VideoArea setPlayer={setPlayer} />
       </div>
       <div className="col-span-1">
-        <ChatBox user={user} setUser={setUser} />
+        <ChatBox user={user} setUser={setUser} player={player} />
       </div>
       <ParticipantFormModal setUser={setUser} />
     </main>
